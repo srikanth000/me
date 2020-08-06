6d917cc9e547d6e1821ecba3ef87a80962b4466c
srikanth <margamsrikanth7@gmail.com>
2020-08-06 10:41:28 +0530

    Updates

---
+++ precache-manifest.8f14e82474ddf73be459b359b3ef8d23.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a107ea237bd7627149e1e1f0ba09aa8",
+    "url": "/personal-web-app/index.html"
+  },
+  {
+    "revision": "93931367ab1db4af970a",
+    "url": "/personal-web-app/static/css/main.634d73c7.chunk.css"
+  },
+  {
+    "revision": "32a70db2fe21d338717d",
+    "url": "/personal-web-app/static/js/2.9bf65e04.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/personal-web-app/static/js/2.9bf65e04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93931367ab1db4af970a",
+    "url": "/personal-web-app/static/js/main.190c02ce.chunk.js"
+  },
+  {
+    "revision": "ec3595593d7fb4bfe5c7",
+    "url": "/personal-web-app/static/js/runtime-main.131d8d9b.js"
+  },
+  {
+    "revision": "c835fee255c34ed83f8a2e0289bc3ed9",
+    "url": "/personal-web-app/static/media/align-left.c835fee2.svg"
+  },
+  {
+    "revision": "11e4ecfb6bc761bc8823a3565d7604bc",
+    "url": "/personal-web-app/static/media/align-right.11e4ecfb.svg"
+  },
+  {
+    "revision": "c517f7a2f142311e01f4eb08a1a9f647",
+    "url": "/personal-web-app/static/media/github.c517f7a2.svg"
+  },
+  {
+    "revision": "ad67420914823952d8bdcdf7275edb71",
+    "url": "/personal-web-app/static/media/linkedin.ad674209.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/personal-web-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68ed5b0e0f312f4b560959eb794d25a8",
+    "url": "/personal-web-app/static/media/message.68ed5b0e.svg"
+  },
+  {
+    "revision": "2445aa22add186b1d50572fc8662cd9c",
+    "url": "/personal-web-app/static/media/profile.2445aa22.svg"
+  },
+  {
+    "revision": "357bebf28185e7fdcff4416697314fd8",
+    "url": "/personal-web-app/static/media/stack-overflow.357bebf2.svg"
+  },
+  {
+    "revision": "47be60baa8a7281a95b735338d2d4b18",
+    "url": "/personal-web-app/static/media/user.47be60ba.svg"
+  },
+  {
+    "revision": "ecf738f2d8d3a92873b19209f1ca1c56",
+    "url": "/personal-web-app/static/media/working-hours.ecf738f2.svg"
+  }
+]);
