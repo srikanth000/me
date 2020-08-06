aca62bc93f630e5f61317df981a7be93f67103d6
srikanth <margamsrikanth7@gmail.com>
2020-08-06 10:39:31 +0530

    Updates

---
+++ precache-manifest.b92c4feef77157d433fcad347c52f63b.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0a839a516409b72465ad0945e0a73d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa2dd08c5925fab1147e",
+    "url": "/static/css/main.634d73c7.chunk.css"
+  },
+  {
+    "revision": "7a7f6a6aa29d28c174fc",
+    "url": "/static/js/2.13bc55f9.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.13bc55f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa2dd08c5925fab1147e",
+    "url": "/static/js/main.b44c2115.chunk.js"
+  },
+  {
+    "revision": "05f526c2489a04a60777",
+    "url": "/static/js/runtime-main.65301d92.js"
+  },
+  {
+    "revision": "c835fee255c34ed83f8a2e0289bc3ed9",
+    "url": "/static/media/align-left.c835fee2.svg"
+  },
+  {
+    "revision": "11e4ecfb6bc761bc8823a3565d7604bc",
+    "url": "/static/media/align-right.11e4ecfb.svg"
+  },
+  {
+    "revision": "c517f7a2f142311e01f4eb08a1a9f647",
+    "url": "/static/media/github.c517f7a2.svg"
+  },
+  {
+    "revision": "ad67420914823952d8bdcdf7275edb71",
+    "url": "/static/media/linkedin.ad674209.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "68ed5b0e0f312f4b560959eb794d25a8",
+    "url": "/static/media/message.68ed5b0e.svg"
+  },
+  {
+    "revision": "2445aa22add186b1d50572fc8662cd9c",
+    "url": "/static/media/profile.2445aa22.svg"
+  },
+  {
+    "revision": "357bebf28185e7fdcff4416697314fd8",
+    "url": "/static/media/stack-overflow.357bebf2.svg"
+  },
+  {
+    "revision": "47be60baa8a7281a95b735338d2d4b18",
+    "url": "/static/media/user.47be60ba.svg"
+  },
+  {
+    "revision": "ecf738f2d8d3a92873b19209f1ca1c56",
+    "url": "/static/media/working-hours.ecf738f2.svg"
+  }
+]);
